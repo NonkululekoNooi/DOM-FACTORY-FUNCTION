03803447d2c4372467cfecfa920904cafac9bcab
NonkululekoNooi <nooi9423@gmail.com>
2022-05-13 15:33:49 +0200

    I have created files

---
+++ js/settings-bill.js
@@ -0,0 +1,60 @@
+
+const button_updateSettings = document.querySelector(".button_updateSettings");
+
+
+const button_primary = document.querySelector(".button_primary");
+
+
+var settings = billWithSettings();
+
+function updateSettings() {
+settings.setCallCost(Number(document.getElementById("callCost").value));
+settings.setSmsCost(Number(document.getElementById("smsCost").value));
+settings.setCriticalLevel(Number(document.getElementById("critical").value));
+settings.setWarningLevel(Number(document.getElementById("warning").value));
+
+colors()
+}
+
+function settingsBillTotal() {
+
+    var checkedRadioBtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
+    if (checkedRadioBtn) {
+        var billItemType = checkedRadioBtn.value
+
+    }
+
+    if (billItemType === "call") {
+       settings.makeCall();
+    }
+    else if (billItemType === "sms") {
+       settings.sendSms();
+    }
+
+    document.getElementById("callTotalSettings").innerHTML = settings.getTotalCallCost().toFixed(2);
+    document.getElementById("smsTotalSettings").innerHTML = settings.getTotalSmsCost().toFixed(2);
+    
+
+    document.getElementById("totalSettings").innerHTML = settings.getTotalCost().toFixed(2);
+
+    colors()
+
+    }
+
+function colors(){
+    document.querySelector(".colour2").classList.remove("warning");
+    document.querySelector(".colour2").classList.remove("danger");
+
+    
+    document.querySelector(".colour2").classList.add(settings.totalClassName()) 
+}
+
+
+
+
+
+
+
+
+
+
---
+++ js/text-bill.js
@@ -0,0 +1,57 @@
+//Get bill item type entererd 'call' or 'sms'
+var billTypeText = document.querySelector(".billTypeText");
+
+//Button to press to add item to bill
+var addToBillBtn =  document.querySelector(".addToBillBtn");
+
+//Where the call total should be displayed
+var callTotalOne = document.querySelector(".callTotalOne");
+
+//Where the sms total should be displayed
+var smsTotalOne = document.querySelector(".smsTotalOne");
+
+
+var callTotalOne=0;
+var smsTotalOne=0;
+
+
+function textBillTotals(){
+
+ // get the value entered in the billType textfield
+ var billTypeEntered = document.getElementById("billTypeText").value;
+ // update the correct total
+ if (billTypeEntered === "call"){
+     callTotalOne += 2.75;
+ }
+ else if (billTypeEntered === "sms"){
+     smsTotalOne += 0.75;
+ }
+ 
+ //update the totals that is displayed on the screen.
+
+ document.getElementById("callTotalOne").innerHTML = callTotalOne.toFixed(2);
+ document.getElementById("smsTotalOne").innerHTML = smsTotalOne.toFixed(2);
+ var totalCost = callTotalOne + smsTotalOne;
+ document.getElementById("totalOne").innerHTML = totalCost.toFixed(2);
+
+
+  
+// //   //color the total based on the criteria
+   if (totalCost >= 50){
+      // adding the danger class will make the text red
+      document.querySelector(".test").classList.add("danger");
+   }
+   else if (totalCost >= 30 && totalCost < 50){
+    document.querySelector(".test").classList.add("warning");
+   
+ }
+
+    }
+   
+
+
+
+
+    
+    
+
